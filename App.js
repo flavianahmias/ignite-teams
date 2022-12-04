07ce4267f2f5b228bd5e0bb35831030bd5aa615d
flavianahmias <flaviaanahmias@gmail.com>
2022-12-03 21:23:05 -0400

    feat: styled components

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
 import Groups from "@screens/Groups";
+import { ThemeProvider } from "styled-components";
+import theme from "@theme/index";
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
+import { Loading } from "@components/Loading";
+
+import { StatusBar } from "react-native";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Container } from "./styles";
 
 export default function Groups() {
-  return (
-    <View style={styles.container}>
-      <Text>Groups!</Text>
-    </View>
-  );
+  return <Container></Container>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
