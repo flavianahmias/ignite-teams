cd7e40c3b717e9e4c99977f7f1a764c30aca59b9
flavianahmias <flaviaanahmias@gmail.com>
2022-12-03 21:30:11 -0400

    feat: add header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+//extensão tabnine
+export const Container = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Logo = styled.Image`
+  width: 46px;
+  height: 56px;
+`;
